1361ef6c1e9e8a2242d2cf9086171c4a30136dc5
semi <semikoder7190@gmail.com>
2022-07-15 17:57:27 +0200

    auto jump for debian based distros

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 alias ls='exa --color=auto'
+set -g theme_color_scheme dracula
+set -g theme_date_format "+%a %H:%M"
+
+fish_add_path /home/semi/.spicetify
+
+begin
+    set --local AUTOJUMP_PATH /usr/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+end
